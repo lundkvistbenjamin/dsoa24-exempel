c80d1f234b19ecdb640af192c2da7b9d64248bb6
”lundkvistbenjamin” <lundkvistbenjamin@gmail.com>
2024-01-15 11:46:11 +0200

    Lektion 3 CC

---
+++ src/Calculator.java
@@ -0,0 +1,10 @@
+public class Calculator {
+
+    public int multiply(int x, int y) {
+        return x * y;
+    }
+
+    public int multiply(int x, int y, int z) {
+        return x * y * z;
+    }
+}
